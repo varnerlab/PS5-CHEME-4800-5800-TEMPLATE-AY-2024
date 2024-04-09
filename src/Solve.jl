330d5bd27180bd94ab7f149ba69447d24b497ecd
Jeffrey Varner <jdv27@cornell.edu>
2024-04-09 12:18:55 -0400

    Small change

--- src/Solve.jl
+++ src/Solve.jl
@@ -1,7 +1,7 @@
 """
 TODO: Add a comment to me. Describe what this function does, what it takes and what it returns.
 """
-function solve(graph::MySimpleDirectedGraphModel, req::Float64)::Dict{String,Any}
+function solve(graph::MySimpleDirectedGraphModel, required::Float64)::Dict{String,Any}
 
     # TODO: impl me!
     throw(ArgumentError("The solve method is not implemented yet!"));
